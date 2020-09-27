d39fe1665db6169494fb7dfaa56f7cca14c23da0
Roman <roman82direct@yandex.ru>
2020-09-27 22:46:42 +0300

    Добавлен класс валидации

--- js/script.js
+++ js/script.js
@@ -10,23 +10,30 @@ goodsList.calcListCost();// не работает
 let goodsInBasket = new BasketList();
 goodsInBasket.renderBasketList();
 
-const nameArea = document.getElementById('name')
-const telArea = document.getElementById('tel')
-const mailArea = document.getElementById('mail')
-
-function check() {
-    (nameArea.value.match(/[^a-zа-яё]/gi) !== null) ? nameArea.style = 'color: red; border-color: red': nameArea.style = 'color: black; border-color: black';
-
-    (telArea.value.match(/^\+7\(\d{3}\)\d{3}-\d{4}$/) == null) ? telArea.style = 'color: red; border-color: red': telArea.style = 'color: black; border-color: black';
-
-    (mailArea.value.match(/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-zа-я]{2,9}$/iu) == null) ? mailArea.style = 'color: red; border-color: red': mailArea.style = 'color: black; border-color: black';
-
+// валидация полей формы
+window.onload = () => {
+    document.getElementById('myform').addEventListener('submit', e => {
+        let valid = new Validator('myform');
+        if(!valid.valid){
+            e.preventDefault();
+        }
+    })
 }
 
-document.querySelector('.contbutt').addEventListener('click', e => {
-    e.preventDefault();
-    check();
-})
+
+// const nameArea = document.getElementById('name')
+// const telArea = document.getElementById('tel')
+// const mailArea = document.getElementById('mail')
+// function check() {
+//     (nameArea.value.match(/[^a-zа-яё]/gi) !== null) ? nameArea.style = 'color: red; border-color: red': nameArea.style = 'color: black; border-color: black';
+//     (telArea.value.match(/^\+?(7|8)(\(|-)?\d{3}(\)|-)?\d{3}-?\d{2}-?\d{2}$/) == null) ? telArea.style = 'color: red; border-color: red': telArea.style = 'color: black; border-color: black';
+//     (mailArea.value.match(/^[a-z0-9._-]+@[a-z0-9._-]+\.[a-zа-я]{2,9}$/iu) == null) ? mailArea.style = 'color: red; border-color: red': mailArea.style = 'color: black; border-color: black';
+// }
+
+// document.querySelector('.contbutt').addEventListener('click', e => {
+//     e.preventDefault();
+//     check();
+// })
 
 // скрипт ниже не работает (прописать методами объекта ProductItem!!!)
 
