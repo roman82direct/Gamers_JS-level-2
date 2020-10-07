bc681a52a5a939501a25808081accaf3b8fb393f
Roman <roman82direct@yandex.ru>
2020-10-07 12:30:51 +0300

    start GamersVue

---
+++ js/ProductComponent.js
@@ -0,0 +1,27 @@
+Vue.component('products', {
+   props: ['products'],
+    methods: {
+        getImg(){
+            let img = products.img;
+            console.log(img)
+        }
+    },
+   template: `<div class="products">
+                <product v-for="item of products" 
+                :key="item.id_product" 
+                :product="item"></product>
+               </div>`
+});
+
+Vue.component('product', {
+    props: ['product', 'img'],
+    template: `
+            <div class="product-item">
+                <img :src="product.img" alt="Some img">
+                <div class="desc">
+                    <h3>{{product.product_name}}</h3>
+                    <p>{{product.price}} руб.</p>
+                    <button class="buy-btn" @click="$parent.$emit('add-product', product)">Купить</button>
+                </div>
+            </div>`
+})
