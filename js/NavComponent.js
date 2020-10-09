bb3fe53ade9542388210fcf7a407467faf69a5fa
Roman <roman82direct@yandex.ru>
2020-10-09 13:36:12 +0300

    Add NavComponent

---
+++ js/NavComponent.js
@@ -0,0 +1,13 @@
+Vue.component ('navcomp', {
+template: ` <nav class="menubox">
+                <ul class="menu clearfix">
+                    <li class="menu-list"><a href="#" class="menu-link">Главная</a></li>
+                    <li class="menu-list"><a href="#" class="menu-link">О нас</a></li>
+                    <li class="menu-list"><a href="#" class="menu-link">Сервис</a></li>
+                    <li class="menu-list"><a href="#" class="menu-link">Галерея</a></li>
+                    <li class="menu-list"><a href="#" class="menu-link">Новости</a></li>
+                    <li class="menu-list"><a href="#" class="menu-link">Команда</a></li>
+                    <li class="menu-list"><a href="#" class="menu-link">Контакты</a></li>
+                </ul>
+            </nav>`
+})
